18e1e1f965990f92a9c6a198f12bcc3453e28342
Zinni Chang <czinni853@gmail.com>
2023-11-26 12:55:38 +0800

    [style]: use Tailwind CDN to make the style

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,23 +1,25 @@
 @extends('layouts.app')
 
-@section('title', 'The list of tasks')
+@section('title', 'Task Lists')
 
 @section('content')
 
-  <div>
-    <a href="{{ route('tasks.create') }}">Add New Task</a>
-  </div>
+  <nav class="my-5">
+    <a href="{{ route('tasks.create') }}" class="text-white font-semibold text-sm bg-emerald-600 p-2 rounded-lg hover:text-black hover:bg-emerald-300  ">Add New Task</a>
+  </nav>
   
   @forelse($tasks as $task)
     <div>
-      <a href="{{ route('tasks.show', ['task' => $task->id]) }}"><span>{{ $task -> title }}</span></a>
+      <a href="{{ route('tasks.show', ['task' => $task->id]) }}" @class(['line-through text-gray-500' => $task->completed])>
+        <span>{{ $task -> title }}</span>
+      </a>
     </div>
   @empty
     <div>There are no tasks!</div>
   @endforelse
 
   @if ($tasks->count())
-    <nav>
+    <nav class="my-5">
       {{ $tasks->links() }}
     </nav>
   @endif
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,10 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Task Lists App</title>
   @yield('styles')
+  <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-  <h1>@yield('title')</h1>
+<body class="container mx-auto mt-10 mb-10 max-w-lg">
+  <h1 class="text-2xl font-bold">@yield('title')</h1>
   <div>
     @if (session()->has('success'))
       <div>{{ session('success') }}</div>
